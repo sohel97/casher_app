a4ec18248a800161ce0c058c603d0182be29c80d
ame5r <ameer.gneem@gmail.com>
2020-08-28 17:22:34 +0300

    save new member data to firebase

--- lib/entities/member.dart
+++ lib/entities/member.dart
@@ -4,11 +4,54 @@ class Member {
   String phoneNumber;
   String city;
   String gender;
-  String idNumber;
   int currentWeight;
   int requestedWeight;
   int height;
   int age;
   DateTime membershipStartDate;
   DateTime membershipEndDate;
+  String uniqueId;
+  Member(
+      String firstName,
+      String lastName,
+      String phoneNumber,
+      String city,
+      String gender,
+      int currentWeight,
+      int requestedWeight,
+      int height,
+      int age,
+      DateTime membershipStartDate,
+      DateTime membershipEndDate,
+      String uniqueId) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.city = city;
+    this.gender = gender;
+    this.currentWeight = currentWeight;
+    this.requestedWeight = requestedWeight;
+    this.height = height;
+    this.age = age;
+    this.membershipStartDate = membershipStartDate;
+    this.membershipEndDate = membershipEndDate;
+    this.uniqueId = uniqueId;
+  }
+
+  getJson() {
+    return {
+      "uniqueId": this.uniqueId,
+      "firstName": this.firstName,
+      "lastName": this.lastName,
+      "phoneNumber": this.phoneNumber,
+      "city": this.city,
+      "gender": this.gender,
+      "currentWeight": this.currentWeight,
+      "requestedWeight": this.requestedWeight,
+      "height": this.height,
+      "age": this.age,
+      "membershipStartDate": this.membershipStartDate.toString(),
+      "membershipEndDate": this.membershipEndDate.toString()
+    };
+  }
 }
--- lib/services/firebaseManagement.dart
+++ lib/services/firebaseManagement.dart
@@ -1,4 +1,22 @@
+import 'dart:math';
+
+import 'package:country_tot_casher/entities/member.dart';
 import 'package:country_tot_casher/screens/addMemberPage.dart';
+import 'package:country_tot_casher/strings.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+Future<bool> addNewMember(
+    DatabaseReference databaseReference, Member newMember) async {
+  DataSnapshot snapshot = await databaseReference.once();
+  databaseReference.push().set(newMember.getJson());
+  if (snapshot.value == null) {
+    print("Member Not exist!");
+  } else {
+    print("Member Already exist!");
+  }
+  return snapshot.value != null;
+}
 
 void addUserToFirebase(
   Gender selectedGender,
@@ -14,6 +32,44 @@ void addUserToFirebase(
   String phoneNumber,
   int perioToAdd,
 ) {
-  print(
-      'selectedGender:$selectedGender \n height:$height \n currentWeight:$currentWeight \n requestedWeight:$requestedWeight  \n birthdayDay:$birthdayDay \n birthdayMonth:$birthdayMonth \n birthdayYear:$birthdayYear \n firstName:$firstName \n lastName:$lastName \n city:$city \n phoneNumber:$phoneNumber \nperioToAdd:$perioToAdd');
+  var newMem = new Member(
+      firstName,
+      lastName,
+      phoneNumber,
+      city,
+      selectedGender.toString(),
+      currentWeight,
+      requestedWeight,
+      height,
+      calculateAge(new DateTime(birthdayYear, birthdayMonth, birthdayDay)),
+      DateTime.now(),
+      new DateTime(DateTime.now().year, DateTime.now().month + perioToAdd,
+          DateTime.now().day),
+      getRandomString(8));
+
+  final ref = FirebaseDatabase().reference().child("Customers");
+  ref.push().set(newMem.getJson());
 }
+
+int calculateAge(DateTime birthDate) {
+  DateTime currentDate = DateTime.now();
+  int age = currentDate.year - birthDate.year;
+  int month1 = currentDate.month;
+  int month2 = birthDate.month;
+  if (month2 > month1) {
+    age--;
+  } else if (month1 == month2) {
+    int day1 = currentDate.day;
+    int day2 = birthDate.day;
+    if (day2 > day1) {
+      age--;
+    }
+  }
+  return age;
+}
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
